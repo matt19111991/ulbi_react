d40934c9bb24dfac6227e1c3edb6c2b38e1d0a42
Dmitry Fursov <mantest@core-tech.ru>
2023-06-23 15:32:53 +0300

    Add eslint rules description

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,41 +22,73 @@ module.exports = {
     '@typescript-eslint',
     'react',
   ],
-
-/* rules:
-      0 === 'off'
-      1 === 'warning'
-      2 === 'error'
+/*
+  rules:
+    0 === 'off'
+    1 === 'warning'
+    2 === 'error'
 */
-
   rules: {
     indent: 0,
-    '@typescript-eslint/no-shadow': 2,
+
+/*  запретить теневое копирование (вкл.)
+    (переменная имеет то же имя, что и переменная в содержащей ее области)
+*/  '@typescript-eslint/no-shadow': 2,
+
+//  ключи enum are defined but never used (откл.)
     '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+
+//   в конце импортов нужно указывать расширения файлов .ts(x) (откл.)
     'import/extensions': [2, 'ignorePackages', { ts: 'never', tsx: 'never' }],
+
+//  webpack и плагины должны быть в dependencies (откл.), т.к. их место в devDependencies
     'import/no-extraneous-dependencies': 0,
+
+//  импорты вида 'export const Sidebar' запрещены, нужен export default (откл.)
     'import/prefer-default-export': 0,
+
+//  в JSX отдается предпочтение одиночным кавычкам (вкл.)
     'jsx-quotes': [2, 'prefer-single'],
+
+//  множество пробелов запрещено (откл.)
     'no-multi-spaces': 0,
+
+//  конфликт c TypeScript, поэтому делегируем в сам TypeScript (@typescript-eslint/no-shadow)
     'no-shadow': 0,
-    'no-underscore-dangle': 0,
-    'no-unused-vars': [2, { varsIgnorePattern: '__IS_DEV__' }],
+
+//  запрет на нижнее подчеркивание в переменных(вкл.)
+    'no-underscore-dangle': [2, { allow: ['__IS_DEV__'] }],
+
+//  нужно явно указывать атрибут 'type' для <button /> (откл.)
     'react/button-has-type': 0,
-    'react/function-component-definition': [1, { namedComponents: 'arrow-function' }],
+
+//  определение функциональных компонентов только через стрелочные функции (вкл.)
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+
+//  нельзя оставить комментарий в JSX (откл.)
     'react/jsx-one-expression-per-line': 0,
+
+//  нельзя прокидывать {...rest} в JSX (откл.)
     'react/jsx-props-no-spreading': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-indent': 2,
+
+//  Разрешить JSX в файлах .jsx и .tsx (вкл.)
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
+
+//  Необходим 'import React from "react"' в каждом файле с JSX (откл.)
     'react/react-in-jsx-scope': 0,
+
+//  Необходимо задавать значения по умолчанию для необязательных props (откл.)
     'react/require-default-props': 0,
   },
 
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.ts', '.tsx'],
-        moduleDirectory: ['src', 'node_modules'],
+// Без 'settings': ошибка 'Unable to resolve path to module'
+
+   settings: {
+     'import/resolver': {
+       node: {
+         extensions: ['.ts', '.tsx'],
+         moduleDirectory: ['src', 'node_modules'],
       },
-    },
-  },
+     },
+   },
 };
