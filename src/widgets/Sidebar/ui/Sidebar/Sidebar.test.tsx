a8d307d6a5b3f74ecc95c0af1aaf575e11ed1905
Dmitry Fursov <mantest@core-tech.ru>
2023-06-29 17:51:58 +0300

    Add component test renderer with routing & translations

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,20 @@
 import { fireEvent, screen } from '@testing-library/react';
 
 import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+  componentTestRenderer,
+} from 'shared/lib/tests/componentTestRenderer/componentTestRenderer';
 
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
   test('Test render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentTestRenderer(<Sidebar />);
 
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('Test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentTestRenderer(<Sidebar />);
 
     const sidebar = screen.getByTestId('sidebar');
 
