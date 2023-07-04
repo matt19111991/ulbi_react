874710fdfd123448c49f83970b0ec9ada79b4043
Dmitry Fursov <mantest@core-tech.ru>
2023-07-04 17:36:24 +0300

    Add screenshots

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
   const { t } = useTranslation();
 
-  const [value, setValue] = useState('');
-
-  const onChange = (val: string): void => {
-    setValue(val);
-  };
-
-  return (
-    <>
-      <div>{t('Главная страница')}</div>
-
-      <Input onChange={onChange} placeholder='Введите текст' value={value} />
-    </>
-  );
+  return <div>{t('Главная страница')}</div>;
 };
 
 export default MainPage;
