e6ea8787f72dd073b4b375d4bb8aa5cdb154000c
Dmitry Fursov <mantest@core-tech.ru>
2023-07-04 15:08:41 +0300

    Add .app className to document.body for storybook

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -6,6 +6,9 @@ import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 // через замыкания пробрасываем тему, затем функцию Story и вызываем её
 
 export const ThemeDecorator = (theme: Theme) => (Story: StoryFn): ReactElement<unknown> => {
+// чтобы не вешать дополнительные классы `${theme}` для 'App.tsx' и порталов
+  document.body.className = theme;
+
   const getThemedStory = () => (
     <ThemeProvider initialTheme={theme}>
       <div className={`app ${theme}`}>
