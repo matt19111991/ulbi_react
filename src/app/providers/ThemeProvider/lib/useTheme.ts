358255a33898828032f19f2dd18d459b290e682e
Dmitry Fursov <mantest@core-tech.ru>
2023-07-15 12:53:59 +0300

    Add orange theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -11,7 +11,25 @@ export function useTheme(): UseThemeResults {
   const { setTheme, theme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT;
+        break;
+
+      case Theme.LIGHT:
+        newTheme = Theme.ORANGE;
+        break;
+
+      case Theme.ORANGE:
+        newTheme = Theme.DARK;
+        break;
+
+      default:
+        newTheme = Theme.LIGHT;
+        break;
+    }
 
     setTheme?.(newTheme); // иначе ошибка "Cannot invoke an object which is possibly 'undefined'"
 
