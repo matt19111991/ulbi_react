68c3b050ceff40c0171becf4350b90359a0f2516
Dmitry Fursov <mantest@core-tech.ru>
2023-07-17 14:21:06 +0300

    Add screenshots

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -2,6 +2,9 @@ import { memo, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
+// import ArticleIcon from 'shared/assets/icons/eye-20-20.svg';
+// import CalendarIcon from 'shared/assets/icons/calendar-20-20.svg';
+
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import {
@@ -11,11 +14,12 @@ import {
 
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
+import { Avatar } from 'shared/ui/Avatar/Avatar';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 import { Text, TextAlign } from 'shared/ui/Text/Text';
 
 import {
-  // getArticleDetailsData,
+  getArticleDetailsData,
   getArticleDetailsError,
   getArticleDetailsIsLoading,
 } from '../../model/selectors/articleDetails';
@@ -39,7 +43,7 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation('article-details');
 
-  // const article = useSelector(getArticleDetailsData);
+  const article = useSelector(getArticleDetailsData);
   const error = useSelector(getArticleDetailsError);
   const isLoading = useSelector(getArticleDetailsIsLoading);
 
@@ -51,19 +55,23 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
 
   if (isLoading) {
     content = (
-      <div>
+      <>
         <Skeleton border='50' className={classes.avatar} height={200} width={200} />
         <Skeleton className={classes.title} height={32} width={300} />
         <Skeleton className={classes.skeleton} height={24} width={600} />
         <Skeleton className={classes.skeleton} height={200} width='100%' />
         <Skeleton className={classes.skeleton} height={200} width='100%' />
-      </div>
+      </>
     );
   } else if (error) {
     content = <Text align={TextAlign.CENTER} title={t('Произошла ошибка при загрузке статьи')} />;
   } else {
-    // eslint-disable-next-line i18next/no-literal-string
-    content = <div>ArticleDetails</div>;
+    content = (
+      <>
+        <Avatar className={classes.avatar} size={200} src={article?.img} />
+        <Text className={classes.title} text={article?.subtitle} title={article?.title} />
+      </>
+    );
   }
 
   return (
