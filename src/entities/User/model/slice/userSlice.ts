86679d3bfc71e97872bed7171080c1a4bb134a65
Dmitry Fursov <mantest@core-tech.ru>
2023-07-20 14:03:35 +0300

    Fix auth data update

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -15,6 +15,8 @@ export const userSlice = createSlice({
 //  сохраняем данные о пользователе после успешного логина
     setAuthData: (state, action: PayloadAction<User>) => {
       state.authData = action.payload;
+
+      window.location.reload(); // хак для обновления информации о пользователе
     },
 //  достаем сохраненные данные о пользователе из localStorage (на случай закрытия вкладки)
     initAuthData: (state) => {
@@ -30,6 +32,8 @@ export const userSlice = createSlice({
       state.authData = undefined;
 
       localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+
+      window.location.reload(); // хак для обновления информации о пользователе
     },
   },
 });
