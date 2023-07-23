a60537e57724c027dfe44a55259c96bd7294a2b0
Dmitry Fursov <mantest@core-tech.ru>
2023-07-23 12:36:09 +0300

    Add ArticleViewSelector stories

---
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider';
+
+import { ArticleView } from 'entities/Article';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { ArticleViewSelector } from './ArticleViewSelector';
+
+const meta = {
+  title: 'features/ArticleViewSelector',
+  component: ArticleViewSelector,
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+} as Meta<typeof ArticleViewSelector>;
+
+type Story = StoryObj<typeof meta>;
+
+// Primary article view selector
+
+export const Primary: Story = {
+  args: {
+    selectedView: ArticleView.LIST,
+  },
+};
+
+// Dark article view selector
+
+export const Dark: Story = {
+  args: {
+    selectedView: ArticleView.LIST,
+  },
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange article view selector
+
+export const Orange: Story = {
+  args: {
+    selectedView: ArticleView.LIST,
+  },
+};
+
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export default meta;
