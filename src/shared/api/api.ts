6b0d5bc0c8e048fef5120f662f4619eab7b493a8
matt19111991 <mantest@core-tech.ru>
2023-07-26 18:42:04 +0300

    Move Authorization header set to interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,18 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '', // проверяется только наличие заголовка
-  },
+});
+
+/*
+  перед каждым запросом будем запускать 'interceptor' и добавлять заголовок для авторизации;
+  если оставить добавление заголовка в 'axios.create' => на сервере могут отправляться пустые
+  значения заголовка 'Authorization'
+*/
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    // проверяется только наличие заголовка
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  }
+
+  return config;
 });
