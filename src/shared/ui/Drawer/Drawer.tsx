fb2a5b267c1a2451a6c07ffa2ce18f1620aef014
matt19111991 <mantest@core-tech.ru>
2023-08-08 22:10:18 +0300

    Add @react-spring/web & @use-gesture/react

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode, useCallback, useEffect } from 'react';
+import { a, config, useSpring } from '@react-spring/web'; // библиотека для анимаций
+import { useDrag } from '@use-gesture/react'; // библиотека для свайпов, тача, drag'n'drop
 
 import { useTheme } from 'app/providers/ThemeProvider';
 
@@ -18,6 +20,8 @@ interface DrawerProps {
   onClose?: () => void;
 }
 
+const height = window.innerHeight - 100; // общая высота окна - 100px
+
 export const Drawer = ({
   children,
   className,
@@ -25,12 +29,68 @@ export const Drawer = ({
   lazy,
   onClose,
 }: DrawerProps) => {
-  const { isMounted, isClosing, onCloseModal } = useModal({ animationDelay: 300, isOpen, onClose });
+  const { isMounted, onCloseModal } = useModal({ animationDelay: 300, isOpen, onClose });
+
+  const [{ y }, api] = useSpring(() => ({ y: height }));
 
   const { theme } = useTheme();
 
+  const openDrawer = useCallback(() => {
+    api.start({ y: 0 }); // запускаем анимацию при открытии 'drawer'
+  }, [api]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDrawer();
+    }
+  }, [isOpen, openDrawer]);
+
+  const close = () => {
+    api.start({ // запускаем анимацию при закрытии 'drawer'
+      config: {
+        ...config.stiff, // вид анимации: { tension: 210, friction: 20 }
+        velocity: 0, // скорость (0 по умолчанию)
+      },
+      onResolve: onClose, // по окончанию анимации вызываем 'onClose();'
+      y: height,
+    });
+  };
+
+  const bind = useDrag(
+    ({
+      cancel,
+      last,
+      direction: [, dy],
+      movement: [, my],
+      velocity: [, vy],
+    }) => {
+      if (my < -70) { // смещение текущего жеста < -70
+        cancel(); // отменяем перетаскивание
+      }
+
+      if (last) { // это последнее событие активного жеста
+        if (my > height * 0.5 || (vy > 0.5 && dy > 0)) { // достаточное смещение для закрытия
+          close(); // закрываем 'drawer'
+        } else {
+          openDrawer(); // без этого повторно не откроется 'drawer'
+        }
+      } else { // это не последнее событие активного жеста
+        api.start({ y: my, immediate: true }); // без анимации выставляем 'y'
+      }
+    },
+    {
+        bounds: { top: 0 }, // границы смещения жеста, может быть 'ref' или DOM узлом
+        from: () => [0, y.get()], // смещение положения начнется с этого значения
+        filterTaps: true, // перетаскивания не будет, если пользователь только что щелкнул компонент
+        rubberband: true, // коэффициент эластичности жеста при выходе за пределы (при 'true' === 0,15)
+      },
+  );
+
+  if (!isOpen) {
+    return null;
+  }
+
   const mods: Mods = {
-    [classes.isClosing]: isClosing,
     [classes.opened]: isOpen,
   };
 
@@ -47,9 +107,17 @@ export const Drawer = ({
       >
         <Overlay onClick={onCloseModal} />
 
-        <div className={classes.content}>
+        <a.div // анимированный 'div'
+          className={classes.sheet}
+          style={{
+            bottom: `calc(-100vh) + ${height - 100}px)`,
+            display: y.to((py) => (py < height ? 'block' : 'none')),
+            y,
+          }}
+          {...bind()}
+        >
           {children}
-        </div>
+        </a.div>
       </div>
     </Portal>
   );
