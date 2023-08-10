285b5047f7757b2126248e6de1964512a1e0bb77
matt19111991 <raven610@yandex.ru>
2023-08-10 11:29:20 +0300

    Finish updateImports script

--- scripts/updateImports.ts
+++ scripts/updateImports.ts
@@ -2,13 +2,32 @@
 
 import { Project } from 'ts-morph';
 
-const project = new Project({}); // проект для работы с AST деревом
+// для работы с AST деревом как с обычными объектами
+const project = new Project({});
 
 project.addSourceFilesAtPaths('src/**/*.ts');  // добавляем все '.ts' файлы
 project.addSourceFilesAtPaths('src/**/*.tsx'); // добавляем все '.tsx' файлы
 
 const files = project.getSourceFiles(); // получаем все эти файлы
 
+const isAbsolutePath = (importPath: string) => {
+  const layers = ['app', 'pages', 'widgets', 'features', 'entities', 'shared'];
+
+  // 'true' для 'entities/Article', 'pages/ArticlesPage', ...
+  return layers.some((layer) => importPath.startsWith(layer));
+};
+
 files.forEach((sourceFile) => {
-  sourceFile.getImportStringLiterals();
+  const importDeclarations = sourceFile.getImportDeclarations(); // получаем все импорты
+
+  importDeclarations.forEach((importDeclaration) => {
+    // importPath => 'entities/Article' | '../../selectors/articlesPageSelectors'
+    const importPath = importDeclaration.getModuleSpecifierValue();
+
+    if (isAbsolutePath(importPath)) {
+      importDeclaration.setModuleSpecifier(`@/${importPath}`);
+    }
+  });
 });
+
+project.save();
