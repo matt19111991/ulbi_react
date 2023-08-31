3ef679f95bb737ab1183b0ef49fe3b12a3d27122
Dmitry Fursov <mantest@core-tech.ru>
2023-08-31 17:32:43 +0300

    Fix deploy.sh

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,8 @@
 # Скрипт для автоматизации действий при деплое (на сервере)
 
+# Нужно дать файлу разрешение на выполнение (на сервере)
+# chmod +x deploy.sh
+
 # идем в папку с проектом
 cd ~/ulbi_react
 
@@ -10,4 +13,4 @@ npm run build:prod
 rm -rf ~/..var/www/ulbi_react/html
 
 # переносим папку с билдом в папку для статики
-mv ~/ulbi_react/build ~/..var/www/ulbi_react/html
+mv ~/ulbi_react/build ~/../var/www/ulbi_react/html
