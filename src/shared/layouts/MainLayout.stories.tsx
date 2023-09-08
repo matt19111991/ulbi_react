d05d70cee896bcca98e3f3026a6cdbce20a4f6e1
Dmitry Fursov <mantest@core-tech.ru>
2023-09-08 10:32:08 +0300

    Add main layout stories

--- src/shared/layouts/MainLayout.stories.tsx
+++ src/shared/layouts/MainLayout.stories.tsx
@@ -21,13 +21,21 @@ type Story = StoryObj<typeof meta>;
 // Primary main layout
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    content: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #D0D0D0', height: '94vh', padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Sidebar</div>,
+  },
 };
 
 // Dark main layout
 
 export const Dark: Story = {
-  args: {},
+  args: {
+    content: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #FFF', height: '94vh', padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Sidebar</div>,
+  },
 };
 
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
@@ -35,9 +43,62 @@ Dark.decorators = [ThemeDecorator(Theme.DARK)];
 // Orange main layout
 
 export const Orange: Story = {
-  args: {},
+  args: {
+    content: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #D0D0D0', height: '94vh', padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Sidebar</div>,
+  },
 };
 
 Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+// Primary main layout with toolbar
+
+export const PrimaryWithToolbar: Story = {
+  args: {
+    content: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #D0D0D0', minWidth: 75, padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Sidebar</div>,
+    toolbar: (
+      <div style={{ border: '1px solid #D0D0D0', height: '90vh', minWidth: 75, padding: 8 }}>
+        Toolbar
+      </div>
+    ),
+  },
+};
+
+// Dark main layout with toolbar
+
+export const DarkWithToolbar: Story = {
+  args: {
+    content: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #FFF', minWidth: 75, padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Sidebar</div>,
+    toolbar: (
+      <div style={{ border: '1px solid #FFF', height: '90vh', minWidth: 75, padding: 8 }}>
+        Toolbar
+      </div>
+    ),
+  },
+};
+
+DarkWithToolbar.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange main layout with toolbar
+
+export const OrangeWithToolbar: Story = {
+  args: {
+    content: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Content</div>,
+    header: <div style={{ border: '1px solid #D0D0D0', minWidth: 75, padding: 8 }}>Header</div>,
+    sidebar: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Sidebar</div>,
+    toolbar: (
+      <div style={{ border: '1px solid #D0D0D0', height: '90vh', minWidth: 75, padding: 8 }}>
+        Toolbar
+      </div>
+    ),
+  },
+};
+
+OrangeWithToolbar.decorators = [ThemeDecorator(Theme.ORANGE)];
 
 export default meta;
