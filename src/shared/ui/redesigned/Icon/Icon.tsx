f648f35c950aa432933a2a985165748f8e0c1c5f
matt19111991 <mantest@core-tech.ru>
2023-09-10 19:35:24 +0300

    Update new icons

--- src/shared/ui/redesigned/Icon/Icon.tsx
+++ src/shared/ui/redesigned/Icon/Icon.tsx
@@ -4,19 +4,57 @@ import { classNames } from '@/shared/lib/classNames/classNames';
 
 import classes from './Icon.module.scss';
 
-interface IconProps extends SVGProps<SVGSVGElement> {
+type SvgProps = Omit<SVGProps<SVGSVGElement>, 'onClick'>;
+
+interface IconBaseProps extends SvgProps {
   className?: string;
-  inverted?: boolean;
   Svg: VFC<SVGProps<SVGSVGElement>>;
 }
 
+interface NonClickableIconProps extends IconBaseProps {
+  clickable?: false;
+}
+
+interface ClickableIconProps extends IconBaseProps {
+  clickable?: true;
+  onClick: () => void;
+}
+
+type IconsProps = NonClickableIconProps | ClickableIconProps;
+
 // Обёртка для SVG (чтобы применялся цвет соответствующей темы к SVG)
 
-export const Icon = memo(({ className, inverted, Svg, ...rest }: IconProps) => (
-  <Svg
-    className={classNames(inverted ? classes.inverted : classes.Icon, {}, [className])}
-    {...rest}
-  />
-));
+export const Icon = memo(
+  ({
+    className,
+    clickable,
+    height = 32,
+    onClick,
+    Svg,
+    width = 32,
+    ...rest
+  }: IconsProps & { onClick?: ClickableIconProps['onClick'] }) => {
+    const icon = (
+      <Svg
+        {...rest}
+        className={classNames(classes.Icon, {}, [className])}
+        height={height}
+        // иначе достается' onClick' из '...rest' и отрабатывает дважды для 'Icon' и 'button'
+        onClick={undefined}
+        width={width}
+      />
+    );
+
+    if (clickable) {
+      return (
+        <button className={classes.button} onClick={onClick} type='button'>
+          {icon}
+        </button>
+      );
+    }
+
+    return icon;
+  },
+);
 
 Icon.displayName = 'Icon';
