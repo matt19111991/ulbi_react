67c96c81d3f985ccd3b58a55c8c21a412251b759
Dmitry Fursov <mantest@core-tech.ru>
2023-09-11 16:09:21 +0300

    Fix redesigned button unit test

--- src/shared/ui/redesigned/Button/Button.test.tsx
+++ src/shared/ui/redesigned/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import { Button, ButtonTheme } from './Button';
+import { Button } from './Button';
 
 describe('Button', () => {
   test('Test render', () => {
@@ -10,7 +10,7 @@ describe('Button', () => {
   });
 
   test('Test clear theme', () => {
-    render(<Button theme={ButtonTheme.CLEAR}>Test</Button>);
+    render(<Button variant='clear'>Test</Button>);
 
     // screen.debug(); // можно посмотреть, что отрисовано в тестовом DOM
 
