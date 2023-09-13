e88c4e794116e03eeefae4b158ad907f6e943248
matt19111991 <mantest@core-tech.ru>
2023-09-13 18:43:26 +0300

    Add sticky content layout

---
+++ src/shared/layouts/StickyContentLayout/StickyContentLayout.stories.tsx
@@ -0,0 +1,81 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Theme } from '@/shared/const/theme';
+
+import { StickyContentLayout } from './StickyContentLayout';
+
+const meta = {
+  title: 'shared/StickyContentLayout',
+  component: StickyContentLayout,
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+} as Meta<typeof StickyContentLayout>;
+
+type Story = StoryObj<typeof meta>;
+
+// Primary sticky content layout
+
+export const Primary: Story = {
+  args: {
+    content: (
+      <div style={{ border: '1px solid #D0D0D0', height: '200vh', padding: 8 }}>Content</div>
+    ),
+    left: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Left</div>,
+    right: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Right</div>,
+  },
+};
+
+// Dark sticky content layout
+
+export const Dark: Story = {
+  args: {
+    content: <div style={{ border: '1px solid #FFF', height: '200vh', padding: 8 }}>Content</div>,
+    left: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Left</div>,
+    right: <div style={{ border: '1px solid #FFF', height: '90vh', padding: 8 }}>Right</div>,
+  },
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange sticky content layout
+
+export const Orange: Story = {
+  args: {
+    content: (
+      <div style={{ border: '1px solid #D0D0D0', height: '200vh', padding: 8 }}>Content</div>
+    ),
+    left: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Left</div>,
+    right: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Right</div>,
+  },
+};
+
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+// Sticky content layout with only left component
+
+export const OnlyLeft: Story = {
+  args: {
+    content: (
+      <div style={{ border: '1px solid #D0D0D0', height: '200vh', padding: 8 }}>Content</div>
+    ),
+    left: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Left</div>,
+  },
+};
+
+// Sticky content layout with only right component
+
+export const OnlyRight: Story = {
+  args: {
+    content: (
+      <div style={{ border: '1px solid #D0D0D0', height: '200vh', padding: 8 }}>Content</div>
+    ),
+    right: <div style={{ border: '1px solid #D0D0D0', height: '90vh', padding: 8 }}>Right</div>,
+  },
+};
+
+export default meta;
---
+++ src/shared/layouts/StickyContentLayout/StickyContentLayout.tsx
@@ -0,0 +1,26 @@
+import { memo, ReactElement } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+import classes from './StickyContentLayout.module.scss';
+
+interface StickyContentLayoutProps {
+  className?: string;
+  content: ReactElement;
+  left?: ReactElement;
+  right?: ReactElement;
+}
+
+export const StickyContentLayout = memo(
+  ({ className, content, left, right }: StickyContentLayoutProps) => (
+    <div className={classNames(classes.StickyContentLayout, {}, [className])}>
+      {left && <div className={classes.left}>{left}</div>}
+
+      <div className={classes.content}>{content}</div>
+
+      {right && <div className={classes.right}>{right}</div>}
+    </div>
+  ),
+);
+
+StickyContentLayout.displayName = 'StickyContentLayout';
