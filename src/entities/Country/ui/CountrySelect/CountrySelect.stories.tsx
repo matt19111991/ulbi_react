1af6a2960dd085ea15ea6a674b4e28c319a42537
Dmitry Fursov <mantest@core-tech.ru>
2023-09-17 17:40:00 +0300

    Add redesigned Country select

--- src/entities/Country/ui/CountrySelect/CountrySelect.stories.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.stories.tsx
@@ -1,10 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { StateSchema } from '@/app/providers/StoreProvider';
+
+import {
+  RedesignDecorator,
+  redesignState,
+} from '@/shared/config/storybook/RedesignDecorator/RedesignDecorator';
+
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 import { Theme } from '@/shared/const/theme';
 
 import { CountrySelect } from './CountrySelect';
+
+const stateCountrySelectRedesigned: DeepPartial<StateSchema> = redesignState;
 
 const meta = {
   title: 'entities/CountrySelect',
@@ -18,26 +28,58 @@ const meta = {
 
 type Story = StoryObj<typeof meta>;
 
-// Primary country select
+// Primary country select old
 
-export const Primary: Story = {
+export const PrimaryOld: Story = {
   args: {},
 };
 
-// Dark country select
+// Dark country select old
 
-export const Dark: Story = {
+export const DarkOld: Story = {
   args: {},
 };
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+DarkOld.decorators = [ThemeDecorator(Theme.DARK)];
 
-// Orange country select
+// Orange country select old
 
-export const Orange: Story = {
+export const OrangeOld: Story = {
   args: {},
 };
 
-Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+OrangeOld.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+// Primary country select new
+
+export const PrimaryNew: Story = {
+  args: {},
+};
+
+PrimaryNew.decorators = [RedesignDecorator, StoreDecorator(stateCountrySelectRedesigned)];
+
+// Dark country select new
+
+export const DarkNew: Story = {
+  args: {},
+};
+
+DarkNew.decorators = [
+  RedesignDecorator,
+  StoreDecorator(stateCountrySelectRedesigned),
+  ThemeDecorator(Theme.DARK),
+];
+
+// Orange country select new
+
+export const OrangeNew: Story = {
+  args: {},
+};
+
+OrangeNew.decorators = [
+  RedesignDecorator,
+  StoreDecorator(stateCountrySelectRedesigned),
+  ThemeDecorator(Theme.ORANGE),
+];
 
 export default meta;
