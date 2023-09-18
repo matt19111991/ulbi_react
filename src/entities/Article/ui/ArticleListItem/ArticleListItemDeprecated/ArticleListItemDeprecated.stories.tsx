f896ecbfda4d7353c470879dc20a2b88b099df79
Dmitry Fursov <mantest@core-tech.ru>
2023-09-18 15:29:47 +0300

    Move ArticleListItem to ArticleListItemDeprecated

---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemDeprecated/ArticleListItemDeprecated.stories.tsx
@@ -0,0 +1,87 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Theme } from '@/shared/const/theme';
+
+import { article } from '@/shared/lib/generators/articles';
+
+import { ArticleView } from '../../../model/consts/articleConsts';
+
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated';
+
+const meta = {
+  title: 'entities/ArticleListItem/old',
+  component: ArticleListItemDeprecated,
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+} as Meta<typeof ArticleListItemDeprecated>;
+
+type Story = StoryObj<typeof meta>;
+
+// Primary article list item
+
+export const PrimaryListItem: Story = {
+  args: {
+    article,
+    view: ArticleView.LIST,
+  },
+};
+
+// Dark article list item
+
+export const DarkListItem: Story = {
+  args: {
+    article,
+    view: ArticleView.LIST,
+  },
+};
+
+DarkListItem.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange article list item
+
+export const OrangeListItem: Story = {
+  args: {
+    article,
+    view: ArticleView.LIST,
+  },
+};
+
+OrangeListItem.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+// Primary article plate item
+
+export const PrimaryPlateItem: Story = {
+  args: {
+    article,
+    view: ArticleView.PLATE,
+  },
+};
+
+// Dark article plate item
+
+export const DarkPlateItem: Story = {
+  args: {
+    article,
+    view: ArticleView.PLATE,
+  },
+};
+
+DarkPlateItem.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange article plate item
+
+export const OrangePlateItem: Story = {
+  args: {
+    article,
+    view: ArticleView.PLATE,
+  },
+};
+
+OrangePlateItem.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export default meta;
---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemDeprecated/ArticleListItemDeprecated.tsx
@@ -0,0 +1,130 @@
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
+
+import { getRouteArticleDetails } from '@/shared/const/router';
+
+// import { useHover } from 'shared/lib/hooks/useHover/useHover';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+import { Avatar } from '@/shared/ui/deprecated/Avatar';
+import { AppLink } from '@/shared/ui/deprecated/AppLink';
+import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button';
+import { Card } from '@/shared/ui/deprecated/Card';
+import { Icon } from '@/shared/ui/deprecated/Icon';
+import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
+import { Text } from '@/shared/ui/deprecated/Text';
+
+import { AppImage } from '@/shared/ui/redesigned/AppImage';
+
+import { ArticleBlockType, ArticleView } from '../../../model/consts/articleConsts';
+
+import { ArticleTextBlock } from '../../../model/types/article';
+
+import { ArticleListItemProps } from '../ArticleListItem';
+import { ArticleTextBlockComponent } from '../../ArticleTextBlockComponent/ArticleTextBlockComponent';
+
+import classes from './ArticleListItemDeprecated.module.scss';
+
+export const ArticleListItemDeprecated = memo(
+  ({ article, className, target, view }: ArticleListItemProps) => {
+    // const [isHover, hoverHandlers] = useHover();
+
+    const { t } = useTranslation();
+
+    if (view === ArticleView.LIST) {
+      const firstTextBlock = article.blocks.find(
+        (block) => block.type === ArticleBlockType.TEXT,
+      ) as ArticleTextBlock;
+
+      return (
+        <div
+          className={classNames('', {}, [className, classes[view]])}
+          data-testid='ArticleListItem'
+        >
+          <Card>
+            <div className={classes.header}>
+              <Avatar size={30} src={article.user.avatar} />
+
+              <Text className={classes.username} text={article.user.username} />
+
+              <Text className={classes.date} text={article.createdAt} />
+            </div>
+
+            <Text className={classes.title} title={article.title} />
+
+            <Text className={classes.types} text={article.type.join(', ')} />
+
+            <AppImage
+              alt={article.title}
+              className={classes.image}
+              loadingFallback={<Skeleton height={250} width='100%' />}
+              src={article.img}
+            />
+
+            {firstTextBlock && (
+              <ArticleTextBlockComponent block={firstTextBlock} className={classes.textBlock} />
+            )}
+
+            <div className={classes.footer}>
+              {/*
+                Для доступности лучше использовать 'AppLink' чем вешать 'onClick' на кнопку
+                Если кликнуть средней кнопкой мыши на кнопку, то в случае с 'onClick' перехода
+                по ссылке не будет. С 'AppLink' переход по ссылке отработает
+              */}
+              <AppLink target={target} to={getRouteArticleDetails(article.id)}>
+                <Button theme={ButtonTheme.OUTLINE}>{t('Читать далее')}...</Button>
+              </AppLink>
+
+              <Text className={classes.views} text={String(article.views || 0)} />
+              <Icon Svg={EyeIcon} />
+            </div>
+          </Card>
+        </div>
+      );
+    }
+
+    /*
+      Для доступности лучше использовать 'AppLink' чем вешать 'onClick' на 'Card'
+      Если кликнуть средней кнопкой мыши на карточку, то в случае с 'onClick' перехода
+      по ссылке не будет. С 'AppLink' переход по ссылке отработает
+    */
+
+    return (
+      <AppLink
+        className={classNames('', {}, [className, classes[view]])}
+        data-testid='ArticleListItem'
+        target={target}
+        to={getRouteArticleDetails(article.id)}
+        /* {...hoverHandlers} */
+      >
+        <Card className={classes.card}>
+          <div className={classes.imageWrapper}>
+            <AppImage
+              alt={article.title}
+              className={classes.image}
+              loadingFallback={<Skeleton height={200} width={200} />}
+              src={article.img}
+            />
+
+            <Text className={classes.date} text={article.createdAt} />
+          </div>
+
+          <div className={classes.infoWrapper}>
+            <Text className={classes.types} text={article.type.join(', ')} />
+
+            <Text className={classes.views} text={String(article.views || 0)} />
+
+            <Icon Svg={EyeIcon} />
+          </div>
+
+          <Text className={classes.title} text={article.title} />
+        </Card>
+      </AppLink>
+    );
+  },
+);
+
+ArticleListItemDeprecated.displayName = 'ArticleListItemDeprecated';
