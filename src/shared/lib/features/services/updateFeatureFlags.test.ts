83f1e4ce7634dd09e534af04420ee89f6dedb667
Dmitry Fursov <mantest@core-tech.ru>
2023-09-19 16:50:58 +0300

    Add updateFeatureFlags tests

---
+++ src/shared/lib/features/services/updateFeatureFlags.test.ts
@@ -0,0 +1,46 @@
+import { TestAsyncThunk } from '../../tests/TestAsyncThunk/TestAsyncThunk';
+
+import { updateFeatureFlags } from './updateFeatureFlags';
+
+describe('updateFeatureFlags', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: {
+        reload: jest.fn(),
+      },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: 'original',
+    });
+  });
+
+  test('success update feature flags', async () => {
+    const thunk = new TestAsyncThunk(updateFeatureFlags);
+
+    const result = await thunk.callThunk({
+      newFeatures: {
+        isAppRedesigned: true,
+      },
+      userId: '1',
+    });
+
+    expect(thunk.dispatch).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('fulfilled');
+    expect(window.location.reload).toHaveBeenCalledTimes(1);
+  });
+
+  test('error update feature flags', async () => {
+    const thunk = new TestAsyncThunk(updateFeatureFlags);
+
+    thunk.api.put.mockReturnValue(Promise.resolve({ status: 403 }));
+
+    const result = await thunk.callThunk();
+
+    expect(result.meta.requestStatus).toBe('rejected');
+  });
+});
---
+++ src/shared/lib/features/services/updateFeatureFlags.ts
@@ -0,0 +1,38 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { ThunkConfig } from '@/app/providers/StoreProvider';
+
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+import { updateFeatureFlagsMutation } from '../api/featureFlagsApi';
+
+import { getAllFeatureFlags } from '../lib/setGetFeatures';
+
+interface UpdateFeatureFlagsOptions {
+  newFeatures: Partial<FeatureFlags>;
+  userId: string;
+}
+
+export const updateFeatureFlags = createAsyncThunk<
+  void,
+  UpdateFeatureFlagsOptions,
+  ThunkConfig<string>
+>('user/saveJsonSettings', async ({ newFeatures, userId }, thunkApi) => {
+  try {
+    await thunkApi.dispatch(
+      updateFeatureFlagsMutation({
+        features: {
+          ...getAllFeatureFlags(),
+          ...newFeatures,
+        },
+        userId,
+      }),
+    );
+
+    window.location.reload();
+  } catch (e) {
+    return thunkApi.rejectWithValue('error');
+  }
+
+  return thunkApi.rejectWithValue('error');
+});
