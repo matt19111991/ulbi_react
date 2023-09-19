cf134cd58e1e1140e9d9d3aeff51b942a9449bf9
Dmitry Fursov <mantest@core-tech.ru>
2023-09-19 16:33:04 +0300

    Add UIDesignSwitcher

---
+++ src/features/UIDesignSwitcher/ui/UIDesignSwitcher/UIDesignSwitcher.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Theme } from '@/shared/const/theme';
+
+import { UIDesignSwitcher } from './UIDesignSwitcher';
+
+const meta = {
+  title: 'features/UIDesignSwitcher',
+  component: UIDesignSwitcher,
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+} as Meta<typeof UIDesignSwitcher>;
+
+type Story = StoryObj<typeof meta>;
+
+// Primary UI design switcher
+
+export const Primary: Story = {
+  args: {},
+};
+
+// Dark UI design switcher
+
+export const Dark: Story = {
+  args: {},
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+// Orange UI design switcher
+
+export const Orange: Story = {
+  args: {},
+};
+
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export default meta;
---
+++ src/features/UIDesignSwitcher/ui/UIDesignSwitcher/UIDesignSwitcher.tsx
@@ -0,0 +1,72 @@
+import { memo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+
+import { getUserAuthData } from '@/entities/User';
+
+import { getFeatureFlag, updateFeatureFlags } from '@/shared/lib/features';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+
+import { ListBox } from '@/shared/ui/redesigned/Popups';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import { HStack } from '@/shared/ui/redesigned/Stack';
+import { Text } from '@/shared/ui/redesigned/Text';
+
+import classes from './UIDesignSwitcher.module.scss';
+
+interface UIDesignSwitcherProps {
+  className?: string;
+}
+
+export const UIDesignSwitcher = memo(({ className }: UIDesignSwitcherProps) => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+
+  const authData = useSelector(getUserAuthData);
+
+  const isAppRedesigned = getFeatureFlag('isAppRedesigned');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const items = [
+    { content: t('Новый'), value: 'new' },
+    { content: t('Старый'), value: 'old' },
+  ];
+
+  const onChange = async (value: string) => {
+    if (authData) {
+      setIsLoading(true);
+
+      await dispatch(
+        updateFeatureFlags({
+          newFeatures: {
+            isAppRedesigned: value === 'new',
+          },
+          userId: authData.id,
+        }),
+      ).unwrap();
+
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <HStack gap='16' max>
+      <Text className={classes.text} text={t('Вариант интерфейса')} />
+
+      {isLoading ? (
+        <Skeleton border='34px' className={classes.skeleton} height={32} width={100} />
+      ) : (
+        <ListBox
+          className={className}
+          items={items}
+          onChange={onChange}
+          stack='horizontal'
+          value={isAppRedesigned ? 'new' : 'old'}
+        />
+      )}
+    </HStack>
+  );
+});
+
+UIDesignSwitcher.displayName = 'UIDesignSwitcher';
