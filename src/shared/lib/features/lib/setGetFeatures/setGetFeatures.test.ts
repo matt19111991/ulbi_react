18b852e1e2ae62e776fb1a58eb814f678364140a
Dmitry Fursov <mantest@core-tech.ru>
2023-09-20 10:06:36 +0300

    Add setGetFeatures tests

---
+++ src/shared/lib/features/lib/setGetFeatures/setGetFeatures.test.ts
@@ -0,0 +1,40 @@
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+import { getAllFeatureFlags, getFeatureFlag, setFeatureFlags } from './setGetFeatures';
+
+const mockFeatureFlags: FeatureFlags = {
+  isAppRedesigned: true,
+  isArticleRatingEnabled: false,
+};
+
+describe('setGetFeatures', () => {
+  afterEach(() => {
+    setFeatureFlags({});
+  });
+
+  test('test set and get feature flags', () => {
+    setFeatureFlags(mockFeatureFlags);
+
+    expect(getAllFeatureFlags()).toEqual(mockFeatureFlags);
+  });
+
+  test('test set and get empty flags', () => {
+    setFeatureFlags();
+
+    expect(getAllFeatureFlags()).toEqual({});
+  });
+
+  describe('getFeatureFlag', () => {
+    test('isAppRedesigned flag should be truthy', () => {
+      setFeatureFlags(mockFeatureFlags);
+
+      expect(getFeatureFlag('isAppRedesigned')).toBeTruthy();
+    });
+
+    test('isArticleRatingEnabled flag should be falsy', () => {
+      setFeatureFlags(mockFeatureFlags);
+
+      expect(getFeatureFlag('isArticleRatingEnabled')).toBeFalsy();
+    });
+  });
+});
