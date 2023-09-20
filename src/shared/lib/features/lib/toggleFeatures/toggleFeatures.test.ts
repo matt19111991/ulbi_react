cd3cd50ab2146d4016b0738d097790a11bc6ab3b
Dmitry Fursov <mantest@core-tech.ru>
2023-09-20 10:14:28 +0300

    Add toggleFeatures tests

---
+++ src/shared/lib/features/lib/toggleFeatures/toggleFeatures.test.ts
@@ -0,0 +1,40 @@
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+import { setFeatureFlags } from '../setGetFeatures/setGetFeatures';
+
+import { toggleFeatures } from './toggleFeatures';
+
+const mockFeatureFlags: FeatureFlags = {
+  isAppRedesigned: true,
+  isArticleRatingEnabled: false,
+};
+
+describe('toggleFeatures', () => {
+  beforeAll(() => {
+    setFeatureFlags(mockFeatureFlags);
+  });
+
+  afterAll(() => {
+    setFeatureFlags({});
+  });
+
+  test('test on', () => {
+    expect(
+      toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => 'on',
+        off: () => 'off',
+      }),
+    ).toBe('on');
+  });
+
+  test('test off', () => {
+    expect(
+      toggleFeatures({
+        name: 'isCounterEnabled',
+        on: () => 'on',
+        off: () => 'off',
+      }),
+    ).toBe('off');
+  });
+});
