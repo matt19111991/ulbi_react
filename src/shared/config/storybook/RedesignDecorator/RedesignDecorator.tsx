00c924245fa2614e8859a67c6cde1cb9b8070368
Dmitry Fursov <mantest@core-tech.ru>
2023-09-22 21:12:24 +0300

    Update screenshots

--- src/shared/config/storybook/RedesignDecorator/RedesignDecorator.tsx
+++ src/shared/config/storybook/RedesignDecorator/RedesignDecorator.tsx
@@ -3,6 +3,8 @@ import type { StoryFn } from '@storybook/react';
 
 import { StateSchema } from '@/app/providers/StoreProvider';
 
+import { LAST_DESIGN_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+
 import { setFeatureFlags } from '@/shared/lib/features';
 
 export const redesignState: DeepPartial<StateSchema> = {
@@ -19,6 +21,8 @@ export const RedesignDecorator = (Story: StoryFn): ReactElement => {
   setFeatureFlags({
     isAppRedesigned: true,
   });
+
+  localStorage.setItem(LAST_DESIGN_LOCALSTORAGE_KEY, 'new');
 
   return <Story />;
 };
