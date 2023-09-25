e1c8ecdd8d5fa0f16034b9b0cfe07d1c240698cb
Dmitry Fursov <mantest@core-tech.ru>
2023-09-25 18:29:12 +0300

    Update yaml

--- src/shared/config/storybook/AsyncDecorator/AsyncDecorator.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-// @ts-ignore
-import createAsyncCallback from '@loki/create-async-callback';
-import { StoryFn } from '@storybook/react';
-
-export const AsyncDecorator = (Story: StoryFn): ReactElement<unknown> => (
-  <Story onDone={createAsyncCallback()} />
-);
