ce53a407179bcaaa99a34b0d62c21362d5a19b96
Dmitry Fursov <mantest@core-tech.ru>
2023-12-16 20:02:08 +0300

    Restore master branch

---
+++ src/webpack/webpack.config.ts
@@ -0,0 +1,30 @@
+import path from 'path';
+
+import { buildWebpack } from './src/webpack/config/build/buildWebpack';
+import { BuildMode, BuildPaths, BuildPlatform } from './src/webpack/config/build/types/types';
+
+interface EnvVariables {
+  analyzer?: boolean;
+  mode?: BuildMode;
+  platform?: BuildPlatform;
+  port?: number;
+}
+
+export default (env: EnvVariables) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'webpack', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    output: path.resolve(__dirname, 'build'),
+    src: path.resolve(__dirname, 'src', 'webpack'),
+  };
+
+  const config = buildWebpack({
+    analyzer: env.analyzer ?? false,
+    mode: env.mode ?? 'development',
+    paths: paths,
+    platform: env.platform ?? 'desktop',
+    port: env.port ?? 3000,
+  });
+
+  return config;
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,29 +1,69 @@
+// const path = require('path'); // Webpack работает в среде Node.js. Есть доступ к 'path'
+
+/* @types/node, @types/webpack и ts-node нужны,
+   чтобы использовать такой формат импортов. Иначе require
+
+   Благодаря модулям и типам выше можно перейти от webpack.config.js к webpack.config.ts
+*/
 import path from 'path';
 
-import { buildWebpack } from './src/webpack/config/build/buildWebpack';
-import { BuildMode, BuildPaths, BuildPlatform } from './src/webpack/config/build/types/types';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
 
-interface EnvVariables {
-  analyzer?: boolean;
-  mode?: BuildMode;
-  platform?: BuildPlatform;
-  port?: number;
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
+
+// используем переменные из '.env' файла; будут доступны в 'process.env.API_URL'
+dotenv.config({ path: './.env' });
+
+/*
+module.exports = { // аналог экспорта для Node.js
+    mode: 'development',
+    ...
 }
+*/
 
-export default (env: EnvVariables) => {
+const getApiUrl = (mode: BuildMode, apiUrl?: string) => {
+  if (apiUrl) {
+    return apiUrl;
+  }
+
+  if (mode === 'production') {
+    return '/api';
+  }
+
+  return 'http://localhost:8000';
+};
+
+// export default config; // если не нужны переменные окружения env, можно вернуть просто config
+
+export default (env: BuildEnv) => {
+  const mode: BuildMode = env?.mode || 'development';
+  const isDev = mode === 'development';
+
+  const apiUrl = getApiUrl(mode, process.env?.API_URL);
+
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'webpack', 'index.tsx'),
+    // __dirname - папка, где находимся в данный момент. В текущем случае: корень
+
+    build: path.resolve(__dirname, 'build'),
+    buildLocales: path.resolve(__dirname, 'build', 'locales'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    favicon: path.resolve(__dirname, 'src', 'shared', 'assets', 'favicon.ico'),
     html: path.resolve(__dirname, 'public', 'index.html'),
-    output: path.resolve(__dirname, 'build'),
-    src: path.resolve(__dirname, 'src', 'webpack'),
+    locales: path.resolve(__dirname, 'public', 'locales'),
+    src: path.resolve(__dirname, 'src'),
   };
 
-  const config = buildWebpack({
-    analyzer: env.analyzer ?? false,
-    mode: env.mode ?? 'development',
-    paths: paths,
-    platform: env.platform ?? 'desktop',
-    port: env.port ?? 3000,
+  const port = env?.port || 3000;
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    apiUrl,
+    isDev,
+    mode,
+    paths,
+    port,
+    project: 'front-end',
   });
 
   return config;
