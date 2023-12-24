6a871ff3ccc8c8cbfb39cf3ba8d94c775f1717bf
Dmitry Fursov <mantest@core-tech.ru>
2023-12-24 17:26:37 +0300

    Migrate to asset modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -33,23 +33,24 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
   const cssLoaders = buildCssLoader(options.isDev);
 
-/* TODO: перейти с file-loader на Asset Modules
-  'raw-loader', 'url-loader' и 'file-loader' можно заменить на Asset Modules (Webpack 5)
+  // 'raw-loader', 'url-loader' и 'file-loader' можно заменить на Asset Modules (Webpack 5)
 
+  // если нужно будет добавить обработку шрифтов, достаточно расширить регулярку "/\.(png|jpe?g|gif|woff)$/i"
   const assetLoader = { // замена для 'file-loader'
     test: /\.(png|jpe?g|gif)$/i, // обрабатывает только PNG, JPG, JPEG и GIF изображения
-    type: 'asset/resource',
+    type: 'asset/resource', // покрывает функционал 'file-loader'
   };
-*/
+
+/*
   const fileLoader = {
-  // если нужно будет добавить обработку шрифтов, достаточно расширить регулярку "/\.(png|jpe?g|gif|woff)$/i"
-  test: /\.(png|jpe?g|gif)$/i, // обрабатывает только PNG, JPG, JPEG и GIF изображения
+    test: /\.(png|jpe?g|gif)$/i,
     use: [
       {
         loader: 'file-loader',
       },
     ],
   };
+*/
 
   const svgLoader = buildSvgLoader();
 
@@ -70,14 +71,16 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   };
 */
   return [ // порядок лоадеров в массиве имеет значение
-    // assetLoader,
-    fileLoader,
+    assetLoader,
+
+    // fileLoader,
+
     svgLoader,
 
     tsBabelLoader,
     tsxBabelLoader,
-
-/* 'babel-loader' нужно использовать для корректной работы React/JSX; сейчас это покрывается 'ts-loader'
+/*
+    'babel-loader' нужно использовать для корректной работы React/JSX; сейчас это покрывается 'ts-loader'
     babelLoader, // должен идти выше typeScriptLoader
 
     typeScriptLoader,
