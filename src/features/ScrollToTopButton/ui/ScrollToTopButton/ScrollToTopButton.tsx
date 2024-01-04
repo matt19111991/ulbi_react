2b8a977d4a02e833a3df611a6ac22743588cd13a
Dmitry Fursov <mantest@core-tech.ru>
2024-01-04 17:46:11 +0300

    Fix scroll to top on responsive devices

--- src/features/ScrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/features/ScrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton.tsx
@@ -3,6 +3,7 @@ import { memo } from 'react';
 import CircleUpIcon from '@/shared/assets/icons/circle-up-redesigned.svg';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { useWindowWidth } from '@/shared/lib/hooks/useWindowWidth/useWindowWidth';
 
 import { Icon } from '@/shared/ui/redesigned/Icon';
 
@@ -13,11 +14,19 @@ interface ScrollToTopButtonProps {
   className?: string;
 }
 
-// TODO: Исправить прокрутку вверх страницы на responsive девайсах
-
 export const ScrollToTopButton = memo(({ className }: ScrollToTopButtonProps) => {
+  const windowWidth = useWindowWidth();
+
   const onClick = () => {
-    window.scrollTo({ behavior: 'smooth', top: 0 });
+    if (windowWidth < 1800) {
+      // прокрутка добавляется к тегу 'main' на разрешениях меньше 1800px
+      const [mainElement] = document.getElementsByTagName('main');
+
+      mainElement.scrollTo({ behavior: 'smooth', top: 0 });
+    } else {
+      // прокрутка добавляется к окну браузера на разрешениях больше 1800px
+      window.scrollTo({ behavior: 'smooth', top: 0 });
+    }
   };
 
   return (
