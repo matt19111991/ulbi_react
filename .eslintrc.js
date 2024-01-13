b491022dc7dab3c6934ff6748c8a7804c50619b9
Dmitry Fursov <mantest@core-tech.ru>
2024-01-13 19:50:04 +0300

    Refactor eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 // Eslint v.8+ работает с WebStorm 2022.2.2+
 
 /*
-  ESLint сообщает только об ошибках своих собственных линтеров, но не сообщает об ошибках компиляции TS.
-  При использовании 'Babel' для более быстрой транспиляции TS кода, типы не проверяются - 'Babel' просто удаляет их
+  'ESLint' сообщает только об ошибках своих собственных линтеров, но не сообщает об ошибках компиляции 'TS'.
+  При использовании 'Babel' для более быстрой транспиляции 'TS' кода, типы не проверяются - 'Babel' просто удаляет их
 */
 
 module.exports = {
+  // каждая среда содержит набор предопределенных глобальных переменных, предоставляем доступ к ним для 'EsLint'
   env: {
     browser: true,
     es2021: true,
     jest: true,
   },
+  // расширяем конфигурацию
   extends: [
     'airbnb',
+    'plugin:@typescript-eslint/recommended',
     'plugin:i18next/recommended',
     'plugin:import/recommended',
+    'plugin:prettier/recommended', // ошибки 'Prettier' передаются 'EsLinty' и подчеркиваются
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
-    'plugin:prettier/recommended', // ошибки Prettier передаются Eslinty и подчеркиваются
-    'plugin:@typescript-eslint/recommended',
   ],
   globals: {
     __API__: 'readonly',
@@ -28,18 +30,24 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+      files: ['**/cypress/**/*.ts'],
       rules: {
-        'i18next/no-literal-string': 0, // отключить правило для тестов и stories
-        'max-len': 0, // отключить правило для тестов и stories
+        // запретить использование 'namespaces' (откл.), для 'cypress' используются 'namespaces'
+        '@typescript-eslint/no-namespace': 0,
       },
     },
     {
       files: ['**/json-server/*.js'],
       rules: {
-        // запретить использование импортов вида "require('path')" (откл.)
-        // отключаем, т.к. такие импорты используются для 'json-server'
+        // запретить импорты вида "require('path')" (откл.), т.к. такие импорты используются для 'json-server'
         '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+    {
+      files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 0, // отключить правило для тестов и stories
+        'max-len': 0, // отключить правило для тестов и stories
       },
     },
   ],
@@ -49,7 +57,7 @@ module.exports = {
       jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module', // т.к. используем ES6 модули
   },
   plugins: [
     '@typescript-eslint',
@@ -60,7 +68,7 @@ module.exports = {
     'react',
     'react-hooks',
 
-    // кастомный плагин на проверку относительных путей (указываем без префикса 'eslint-plugin')
+    // кастомный плагин для проверки относительных путей (указываем без префикса 'eslint-plugin')
     'path-checker-1911',
 
     'unused-imports', // проверяет неиспользуемые импорты
@@ -74,46 +82,46 @@ module.exports = {
   */
 
   rules: {
-    // запретить использование namespaces (выкл.), для 'cypress' используются namespaces
-    '@typescript-eslint/no-namespace': 0,
-
-    // запретить теневое копирование (вкл.)
-    // (переменная имеет то же имя, что и переменная в содержащей ее области)
+    // запретить теневое копирование (вкл.): переменная имеет то же имя, что и переменная в содержащей ее области
+    // let a = 3;
+    // function b() {
+    //   let a = 10;
+    // }
     '@typescript-eslint/no-shadow': 2,
 
-    // в 'useEffect' должны использоваться стрелочные функции (откл.)
-    'arrow-body-style': 0,
-
-    // ключи enum are defined but never used (откл.)
+    // переменная объявлена, но не используется (откл. для аргументов в функциях)
     '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
 
-    // переводы и "t('Главная страница')" должны быть только в JSX (вкл.)
-    // и должны игнорироваться атрибуты 'data-testid' и 'to' (ссылки) (вкл.)
+    // при возврате значения из функции избегаем использования 'return'; значение возвращаем сразу без '{}' (откл.)
+    'arrow-body-style': 0,
+
+    // переводы "t('Главная страница')" должны быть только в 'JSX' (вкл.)
+    // должны игнорироваться атрибуты 'data-testid' и 'to' (ссылки) (вкл.)
     'i18next/no-literal-string': [2, { ignoreAttribute: ['data-testid', 'to'], markupOnly: true }],
 
-    // в конце импортов нужно указывать расширения файлов .ts(x) (откл.)
+    // в конце импортов нужно указывать расширения файлов '.ts(x)' (откл.); правило включено для 'node_modules'
     'import/extensions': [2, 'ignorePackages', { ts: 'never', tsx: 'never' }],
 
-    // webpack и плагины должны быть в dependencies (откл.), т.к. их место в devDependencies
+    // 'Webpack' и плагины должны быть в 'dependencies' (откл.), т.к. их место в 'devDependencies'
     'import/no-extraneous-dependencies': 0,
 
     // относительные импорты запрещены (откл.)
     'import/no-relative-packages': 0,
 
-    // импорты вида 'export const Sidebar' запрещены; нужен export default (откл.)
+    // импорты вида 'export const Sidebar' запрещены; нужен 'export default' (откл.)
     'import/prefer-default-export': 0,
 
-    //  проверка отступов (откл.)
-    // indent: 0,
+    // проверка отступов (откл.)
+    indent: 0,
 
-    // проверка семантики (div помимо onClick обработчика
-    // должен иметь хотя бы один keyboard обработчик) (откл.)
+    // проверка семантики ('div' помимо 'onClick' обработчика
+    // должен иметь хотя бы один 'keyboard' обработчик) (откл.)
     'jsx-a11y/click-events-have-key-events': 0,
 
-    // проверка семантики (div не должен иметь onClick обработчиков или должна быть указана 'role') (откл.)
+    // проверка семантики ('div' не должен иметь 'onClick' обработчиков или должна быть указана 'role') (откл.)
     'jsx-a11y/no-static-element-interactions': 0,
 
-    // в JSX отдается предпочтение одинарным кавычкам (вкл.)
+    // в 'JSX' отдается предпочтение одинарным кавычкам (вкл.)
     'jsx-quotes': [2, 'prefer-single'],
 
     // проверка на корректность символов перевода строки (откл.):
@@ -130,26 +138,25 @@ module.exports = {
     // множество пробелов запрещено (откл.)
     'no-multi-spaces': 0,
 
-    // запрещена мутация данных (переназначение параметров функции) (откл.)
+    // запрещена мутация данных (переназначение параметров функции, изменение 'state' в слайсах) (откл.)
     'no-param-reassign': 0,
 
     // запрещен инкремент (i++) (откл.)
     'no-plusplus': 0,
 
-    // конфликт c TypeScript, поэтому делегируем в сам TypeScript (@typescript-eslint/no-shadow)
+    // конфликт c 'TypeScript', поэтому делегируем в сам 'TypeScript' (@typescript-eslint/no-shadow) (откл.)
     'no-shadow': 0,
 
-    // запрет на нижнее подчеркивание в переменных(вкл.)
+    // запрет на нижнее подчеркивание в переменных (вкл.)
     'no-underscore-dangle': [2, { allow: ['__API__', '__IS_DEV__', '__PROJECT__'] }],
 
-    // чтобы была возможность использовать кастомный 'DeepPartial' и TypeScript не ругался
-    // на объявление глобальных переменных, var и т.д.
+    // чтобы была возможность использовать кастомный 'DeepPartial' и 'TypeScript' не ругался
+    // на объявление глобальных переменных, 'var' и т.д.
     'no-undef': 0,
 
-    /*
-      отключаем для корректной работы с Eslint v.7, т.к. сыпет ошибками по всем файлам
-      'no-unsafe-optional-chaining': 0,
-    */
+    // запрещаем конструкции 'obj?.foo' (откл.)
+    // отключаем для корректной работы с Eslint v.7, т.к. сыпет ошибками по всем файлам
+    // 'no-unsafe-optional-chaining': 0,
 
     // кастомное правило для проверки относительных путей (вкл.)
     'path-checker-1911/path-checker': [2, { alias: '@' }],
@@ -160,7 +167,7 @@ module.exports = {
       {
         alias: '@',
 
-        // игнорируем правило для тестовых файлов, файлов storybook и StoreDecorator
+        // игнорируем правило для тестовых файлов, файлов 'storybook' и 'StoreDecorator'
         testFilesPatterns: ['**/*.test.*', '**/*.stories.*', '**/StoreDecorator.tsx'],
       },
     ],
@@ -170,6 +177,7 @@ module.exports = {
       2,
       {
         alias: '@',
+
         // игнорируем правило для путей внутри генераторов, декораторов, роутера и тестовой среды
         ignoreFilePathPatterns: [
           '**/generators/**',
@@ -185,12 +193,8 @@ module.exports = {
       },
     ],
 
-    // нужно явно указывать атрибут 'type' для <button /> (откл.)
+    // нужно явно указывать атрибут 'type' для '<button />' (откл.)
     'react/button-has-type': 0,
-
-    // задаем максимально допустимое количество пропсов в одну строку в JSX (вкл.),
-    // если пропсов больше => каждый prop будет на новой строке
-    'react/jsx-max-props-per-line': [2, { maximum: 5 }],
 
     // определение функциональных компонентов только через стрелочные функции (вкл.)
     'react/function-component-definition': [
@@ -201,6 +205,31 @@ module.exports = {
       },
     ],
 
+    // разрешить 'JSX' в файлах '.jsx' и '.tsx' (вкл.)
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
+
+    // базовые отступы в 'JSX': 2 пробела (2-ой элемент массива) (вкл.)
+    'react/jsx-indent': [2, 2],
+
+    // отступы для 'props' в 'JSX': 2 пробела (2-ой элемент массива) (вкл.)
+    'react/jsx-indent-props': [2, 2],
+
+    // задаем максимально допустимое количество пропсов в одну строку в 'JSX' (вкл.),
+    // если пропсов больше => каждый 'prop' будет на новой строке
+    'react/jsx-max-props-per-line': [2, { maximum: 5 }],
+
+    // нельзя оставить комментарий в 'JSX' (откл.)
+    'react/jsx-one-expression-per-line': 0,
+
+    // нельзя прокидывать '{...rest}' в 'JSX' (откл.)
+    'react/jsx-props-no-spreading': 0,
+
+    // необходим 'import React from "react"' в каждом файле с 'JSX' (откл.)
+    'react/react-in-jsx-scope': 0,
+
+    // необходимо задавать значения по умолчанию для необязательных 'props' (откл.)
+    'react/require-default-props': 0,
+
     // в массиве зависимостей хука должны быть указаны все переменные и функции,
     // которые используются в текущем хуке (вкл.)
     'react-hooks/exhaustive-deps': 2,
@@ -208,35 +237,14 @@ module.exports = {
     // проверка главных правил использования hooks (вкл.)
     'react-hooks/rules-of-hooks': 2,
 
-    // разрешить JSX в файлах .jsx и .tsx (вкл.)
-    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
-
-    // базовые отступы в JSX: 2 пробела (2-ой элемент массива)
-    // 'react/jsx-indent': [2, 2],
-
-    // отступы для props в JSX: 2 пробела (2-ой элемент массива)
-    // 'react/jsx-indent-props': [2, 2],
-
-    // нельзя оставить комментарий в JSX (откл.)
-    'react/jsx-one-expression-per-line': 0,
-
-    // нельзя прокидывать {...rest} в JSX (откл.)
-    'react/jsx-props-no-spreading': 0,
-
-    // необходим 'import React from "react"' в каждом файле с JSX (откл.)
-    'react/react-in-jsx-scope': 0,
-
-    // необходимо задавать значения по умолчанию для необязательных props (откл.)
-    'react/require-default-props': 0,
-
     // ошибка при нахождении неиспользуемых импортов (вкл.)
     'unused-imports/no-unused-imports': 2,
   },
   /*
     для работы абсолютных импортов вида '@/*' и чтобы не указывать расширения для файлов,
     нужно установить 'eslint-import-resolver-alias' и добавить 'settings', иначе ошибки:
-        - 'Missing file extension for "@/shared/lib/..." (import/extensions)'
-        - 'Unable to resolve path to module "@/shared/lib/..." (import/no-unresolved)'
+      - 'Missing file extension for "@/shared/lib/..." (import/extensions)'
+      - 'Unable to resolve path to module "@/shared/lib/..." (import/no-unresolved)'
   */
   settings: {
     'import/resolver': {
