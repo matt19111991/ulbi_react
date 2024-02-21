0b6781c1fdd723188f0f75a0fb43aa7807bc0b19
Dmitry Fursov <mantest@core-tech.ru>
2024-02-21 17:59:44 +0300

    Refactor buildSelector

--- src/entities/User/model/selectors/getUserJsonSettings/getUserJsonSettings.ts
+++ src/entities/User/model/selectors/getUserJsonSettings/getUserJsonSettings.ts
@@ -2,9 +2,11 @@ import { buildSelector } from '@/shared/lib/store';
 
 import { JsonSettings } from '../../types/jsonSettings';
 
+type Arguments = never; // 'useJsonSettings()' не принимает аргументов
+
 // выносим за пределы селектора, чтобы константа не менялась
 const defaultJsonSettings: JsonSettings = {};
 
-export const [useJsonSettings, getJsonSettings] = buildSelector(
+export const [useJsonSettings, getJsonSettings] = buildSelector<JsonSettings, Arguments>(
   (state) => state.user?.authData?.jsonSettings ?? defaultJsonSettings,
 );
