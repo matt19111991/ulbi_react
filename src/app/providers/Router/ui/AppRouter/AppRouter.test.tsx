c7c3b888b5f06dcc4c75af0622002c12dc9da0d7
matt19111991 <mantest@core-tech.ru>
2024-03-28 18:58:14 +0300

    Fix router unit tests

--- src/app/providers/Router/ui/AppRouter/AppRouter.test.tsx
+++ src/app/providers/Router/ui/AppRouter/AppRouter.test.tsx
@@ -1,5 +1,7 @@
 import { screen } from '@testing-library/react';
 
+import type { StateSchema } from '@/app/providers/StoreProvider';
+
 import { UserRole } from '@/entities/User/testing';
 
 import { getRouteAbout, getRouteAdmin, getRouteProfile } from '@/shared/const/router';
@@ -10,86 +12,117 @@ import { AppRouter } from './AppRouter';
 
 describe('AppRouter', () => {
   test('Страница должна отрисовываться', async () => {
-    componentTestRenderer(<AppRouter />, { route: getRouteAbout() });
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        mounted: true,
+      },
+    };
+
+    componentTestRenderer(<AppRouter />, {
+      initialState,
+      route: getRouteAbout(),
+    });
 
     // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('AboutPage');
 
     /*
-      Нужна актуальная версия '@testing-library', которая соответствует 'React v.18', иначе ошибка:
-      'Unable to find an element by: [data-testid="AboutPage"]'
+      нужна актуальная версия '@testing-library', которая соответствует 'React v.18', иначе ошибка:
+     'Unable to find an element by: [data-testid="AboutPage"]'
     */
     expect(page).toBeInTheDocument();
   });
 
   test('Страница не найдена', async () => {
-    componentTestRenderer(<AppRouter />, { route: '/not_exist' });
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        mounted: true,
+      },
+    };
 
+    componentTestRenderer(<AppRouter />, {
+      initialState,
+      route: '/not_exist',
+    });
+
+    // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('NotFoundPage');
 
     expect(page).toBeInTheDocument();
   });
 
   test('Редирект неавторизованного пользователя на главную страницу', async () => {
-    componentTestRenderer(<AppRouter />, {
-      initialState: {
-        user: {
-          mounted: true,
-        },
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        mounted: true,
       },
+    };
+
+    componentTestRenderer(<AppRouter />, {
+      initialState,
       route: getRouteProfile('1'),
     });
 
+    // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('MainPage');
 
     expect(page).toBeInTheDocument();
   });
 
   test('Доступ к закрытой странице для авторизованного пользователя', async () => {
-    componentTestRenderer(<AppRouter />, {
-      initialState: {
-        user: {
-          authData: {},
-          mounted: true,
-        },
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        authData: {},
+        mounted: true,
       },
+    };
+
+    componentTestRenderer(<AppRouter />, {
+      initialState,
       route: getRouteProfile('1'),
     });
 
+    // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('ProfilePage');
 
     expect(page).toBeInTheDocument();
   });
 
   test('Доступ запрещен (отсутствует роль)', async () => {
-    componentTestRenderer(<AppRouter />, {
-      initialState: {
-        user: {
-          authData: {},
-          mounted: true,
-        },
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        authData: {},
+        mounted: true,
       },
+    };
+
+    componentTestRenderer(<AppRouter />, {
+      initialState,
       route: getRouteAdmin(),
     });
 
+    // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('ForbiddenPage');
 
     expect(page).toBeInTheDocument();
   });
 
   test('Доступ разрешен (присутствует роль)', async () => {
-    componentTestRenderer(<AppRouter />, {
-      initialState: {
-        user: {
-          authData: {
-            roles: [UserRole.ADMIN],
-          },
-          mounted: true,
+    const initialState: DeepPartial<StateSchema> = {
+      user: {
+        authData: {
+          roles: [UserRole.ADMIN],
         },
+        mounted: true,
       },
+    };
+
+    componentTestRenderer(<AppRouter />, {
+      initialState,
       route: getRouteAdmin(),
     });
 
+    // используем асинхронный 'findByTestId', т.к. роутер использует 'Suspense'
     const page = await screen.findByTestId('AdminPanelPage');
 
     expect(page).toBeInTheDocument();
