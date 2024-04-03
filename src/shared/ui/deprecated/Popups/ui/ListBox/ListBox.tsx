c1915dd818aadb672cf87f39add9f7a9046be05d
matt19111991 <mantest@core-tech.ru>
2024-04-03 18:34:03 +0300

    Remove generic memo from deprecated listbox

--- src/shared/ui/deprecated/Popups/ui/ListBox/ListBox.tsx
+++ src/shared/ui/deprecated/Popups/ui/ListBox/ListBox.tsx
@@ -1,10 +1,9 @@
-import { Fragment, ReactNode } from 'react';
+import { Fragment, memo, ReactNode } from 'react';
 import { Listbox as HeadlessListBox } from '@headlessui/react';
 
 import CheckIcon from '@/shared/assets/icons/check-16-12.svg';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { genericMemo } from '@/shared/lib/components/genericMemo/genericMemo';
 
 import { DropdownDirection } from '@/shared/types/ui';
 
@@ -80,65 +79,65 @@ interface ListBoxProps {
  * Устарел, используем новые компоненты из папки 'redesigned'
  * @deprecated
  */
-const ListBox = ({
-  className,
-  defaultValue,
-  direction = 'bottom-left',
-  items,
-  label,
-  onChange,
-  readonly,
-  value,
-}: ListBoxProps) => (
-  <VStack align='start' gap='4'>
-    {label && <span>{`${label} >`}</span>}
+export const ListBox = memo(
+  ({
+    className,
+    defaultValue,
+    direction = 'bottom-left',
+    items,
+    label,
+    onChange,
+    readonly,
+    value,
+  }: ListBoxProps) => (
+    <VStack align='start' gap='4'>
+      {label && <span>{`${label} >`}</span>}
 
-    <HeadlessListBox
-      as='div'
-      className={classNames(popupClasses.popup, {}, [className])}
-      disabled={readonly}
-      onChange={onChange}
-      value={value}
-    >
-      <HeadlessListBox.Button as='div' className={classes.trigger}>
-        <Button disabled={readonly}>{value ?? defaultValue}</Button>
-      </HeadlessListBox.Button>
-
-      <HeadlessListBox.Options
-        className={classNames(classes.options, {}, [mapDirectionClass[direction]])}
+      <HeadlessListBox
+        as='div'
+        className={classNames(popupClasses.popup, {}, [className])}
+        disabled={readonly}
+        onChange={onChange}
+        value={value}
       >
-        {items?.map((item) => (
-          <HeadlessListBox.Option
-            as={Fragment}
-            disabled={item.disabled}
-            key={item.value}
-            value={item.value}
-          >
-            {({ active, selected }) => (
-              <li
-                className={classNames(
-                  classes.item,
-                  {
-                    [popupClasses.active]: active,
-                    [popupClasses.disabled]: item.disabled,
-                  },
-                  [],
-                )}
-              >
-                <HStack gap='4'>
-                  {item.content}
+        <HeadlessListBox.Button as='div' className={classes.trigger}>
+          <Button disabled={readonly}>{value ?? defaultValue}</Button>
+        </HeadlessListBox.Button>
 
-                  {selected && <CheckIcon className={classes.check} />}
-                </HStack>
-              </li>
-            )}
-          </HeadlessListBox.Option>
-        ))}
-      </HeadlessListBox.Options>
-    </HeadlessListBox>
-  </VStack>
+        <HeadlessListBox.Options
+          className={classNames(classes.options, {}, [mapDirectionClass[direction]])}
+        >
+          {items?.map((item) => (
+            <HeadlessListBox.Option
+              as={Fragment}
+              disabled={item.disabled}
+              key={item.value}
+              value={item.value}
+            >
+              {({ active, selected }) => (
+                <li
+                  className={classNames(
+                    classes.item,
+                    {
+                      [popupClasses.active]: active,
+                      [popupClasses.disabled]: item.disabled,
+                    },
+                    [],
+                  )}
+                >
+                  <HStack gap='4'>
+                    {item.content}
+
+                    {selected && <CheckIcon className={classes.check} />}
+                  </HStack>
+                </li>
+              )}
+            </HeadlessListBox.Option>
+          ))}
+        </HeadlessListBox.Options>
+      </HeadlessListBox>
+    </VStack>
+  ),
 );
 
-const MemoizedListBox = genericMemo(ListBox);
-
-export { MemoizedListBox as ListBox };
+ListBox.displayName = 'ListBox';
