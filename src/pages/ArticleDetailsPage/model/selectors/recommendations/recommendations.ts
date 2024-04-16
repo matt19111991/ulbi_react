693439949cf347bc4322d5c6a8d687b3b19aa055
Dmitry Fursov <mantest@core-tech.ru>
2024-04-16 15:13:26 +0300

    Remove getArticleRecommendationsAreLoading selector

--- src/pages/ArticleDetailsPage/model/selectors/recommendations/recommendations.ts
+++
@@ -1,4 +0,0 @@
-import { StateSchema } from '@/app/providers/StoreProvider';
-
-export const getArticleRecommendationsAreLoading = (state: StateSchema) =>
-  state.articleDetailsPage?.recommendations?.areLoading;
