b278165d4afc78e373e9ab6981c1c53bb27868e3
Dmitry Fursov <mantest@core-tech.ru>
2024-04-28 12:44:42 +0300

    Update getProfileData selector

--- src/features/EditableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
+++ src/features/EditableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
@@ -1,17 +1,22 @@
-import { StateSchema } from '@/app/providers/StoreProvider';
+import type { StateSchema } from '@/app/providers/StoreProvider';
 
 import { Country } from '@/entities/Country/testing';
 import { Currency } from '@/entities/Currency/testing';
+
+import type { ProfileSchema } from '../../types/editableProfileCardSchema';
 
 import { getProfileData } from './getProfileData';
 
 describe('getProfileData', () => {
   test('should return data', () => {
-    const data = {
+    const data: ProfileSchema['data'] = {
       age: 22,
+      avatar:
+        'https://ik.imagekit.io/ably/ghost/prod/2023/12/choosing-the-best-javascript-frameworks-for-your-next-project.png?tr=w-1728,q-50',
       city: 'New-York',
       country: Country.USA,
       currency: Currency.USD,
+      id: '1',
       first: 'Jack',
       lastname: 'Smith',
       username: 'admin',
@@ -27,6 +32,6 @@ describe('getProfileData', () => {
   test('should work with empty state', () => {
     const state: DeepPartial<StateSchema> = {};
 
-    expect(getProfileData(state as StateSchema)).toBe(undefined);
+    expect(getProfileData(state as StateSchema)).toBeUndefined();
   });
 });
