fae5910e8ac8af08331a77e672373ce0229dc968
Dmitry Fursov <mantest@core-tech.ru>
2024-04-28 12:56:29 +0300

    Refactor getProfileForm

--- src/features/EditableProfileCard/model/selectors/getProfileForm/getProfileForm.test.ts
+++ src/features/EditableProfileCard/model/selectors/getProfileForm/getProfileForm.test.ts
@@ -1,17 +1,22 @@
-import { StateSchema } from '@/app/providers/StoreProvider';
+import type { StateSchema } from '@/app/providers/StoreProvider';
 
 import { Country } from '@/entities/Country/testing';
 import { Currency } from '@/entities/Currency/testing';
+
+import type { ProfileSchema } from '../../types/editableProfileCardSchema';
 
 import { getProfileForm } from './getProfileForm';
 
 describe('getProfileForm', () => {
   test('should return form', () => {
-    const form = {
+    const form: ProfileSchema['form'] = {
       age: 22,
+      avatar:
+        'https://ik.imagekit.io/ably/ghost/prod/2023/12/choosing-the-best-javascript-frameworks-for-your-next-project.png?tr=w-1728,q-50',
       city: 'New-York',
       country: Country.USA,
       currency: Currency.USD,
+      id: '1',
       first: 'Jack',
       lastname: 'Smith',
       username: 'admin',
@@ -27,6 +32,6 @@ describe('getProfileForm', () => {
   test('should work with empty state', () => {
     const state: DeepPartial<StateSchema> = {};
 
-    expect(getProfileForm(state as StateSchema)).toBe(undefined);
+    expect(getProfileForm(state as StateSchema)).toBeUndefined();
   });
 });
