f6dbbeef2a97be0b8e2366930e4a9ba453a81820
Dmitry Fursov <mantest@core-tech.ru>
2024-05-03 20:38:44 +0300

    Refactor buildSlice

--- src/shared/lib/store/buildSlice/buildSlice.ts
+++ src/shared/lib/store/buildSlice/buildSlice.ts
@@ -1,37 +1,51 @@
 import { useMemo } from 'react';
-import { useDispatch } from 'react-redux';
+import { bindActionCreators, createSlice } from '@reduxjs/toolkit';
 
-import {
-  bindActionCreators,
-  createSlice,
+import type {
+  CaseReducerActions,
   CreateSliceOptions,
   SliceCaseReducers,
+  SliceSelectors,
+  Slice,
 } from '@reduxjs/toolkit';
+
+import { useAppDispatch } from '../../hooks/useAppDispatch/useAppDispatch';
+
+// все 'Generics' и типы взяты из исходников 'createSlice'
+
+interface UseActions<State, CaseReducers extends SliceCaseReducers<State>, Name extends string> {
+  useActions: () => CaseReducerActions<CaseReducers, Name>;
+}
+
+type Return<
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string,
+  ReducerPath extends string,
+  Selectors extends SliceSelectors<State>,
+> = Slice<State, CaseReducers, Name, ReducerPath, Selectors> &
+  UseActions<State, CaseReducers, Name>;
 
 /**
  * Аналог 'createSlice' из '@reduxjs/toolkit', но без необходимости вызывать 'dispatch'
  * каждый раз в компонентах
  * @param options
  */
-
 export const buildSlice = <
   State,
   CaseReducers extends SliceCaseReducers<State>,
-  Name extends string = string,
+  Name extends string,
+  Selectors extends SliceSelectors<State>,
+  ReducerPath extends string = Name,
 >(
-  options: CreateSliceOptions<State, CaseReducers, Name>,
-) => {
+  options: CreateSliceOptions<State, CaseReducers, Name, ReducerPath, Selectors>,
+): Return<State, CaseReducers, Name, ReducerPath, Selectors> => {
   const slice = createSlice(options);
 
-  // 'typeof slice.actions' для корректной типизации
-  const useActions = (): typeof slice.actions => {
-    const dispatch = useDispatch();
+  const useActions = () => {
+    const dispatch = useAppDispatch();
 
-    /*
-      биндим 'dispatch' к каждому экшену, чтобы 'dispatch'
-      не приходилось вызывать каждый раз в компонентах
-   */
-
+    // биндим 'dispatch' к каждому экшену, чтобы 'dispatch' не приходилось вызывать каждый раз в компонентах
     return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch]);
   };
 
