8347d4d32934b20c4c6986fd735de75c8a904cc9
Dmitry Fursov <mantest@core-tech.ru>
2024-05-16 16:25:05 +0300

    Refactor serviceTemplate

---
+++ scripts/generateFsdSliceTemplate/templates/service/index.ts
@@ -0,0 +1,28 @@
+export const serviceTemplate = (sliceName: string) =>
+  `import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import type { ThunkConfig } from '@/app/providers/StoreProvider';
+
+import type { ResponseEntity } from '@/entities/${sliceName}';
+
+import type { RequestArguments } from '../../types/${sliceName}Schema';
+
+export const ${sliceName}Service = createAsyncThunk<ResponseEntity, RequestArguments, ThunkConfig<string>>(
+  '${sliceName}/${sliceName}Service',
+  async (args, thunkApi) => {
+    try {
+      const state = thunkApi.getState();
+
+      const response = await thunkApi.extra.api.post<ResponseEntity>('/${sliceName}Url', ['123']);
+
+      if (!response.data) {
+        return thunkApi.rejectWithValue('No ${sliceName} data');
+      }
+
+      return response.data;
+    } catch (e) {
+      return thunkApi.rejectWithValue(e instanceof Error ? e.message : 'Unexpected error');
+    }
+  },
+);
+`;
