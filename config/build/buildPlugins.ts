d19c0a8d93a6dac6078504409996485e795c5750
matt19111991 <raven610@yandex.ru>
2024-08-21 12:46:32 +0300

    Change service worker path

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -133,7 +133,7 @@ export function buildPlugins({
           { from: paths.sitemapIndex, to: paths.build },
           { from: paths.sitemap1, to: paths.build },
           { from: paths.sitemap2, to: paths.build },
-          { from: path.resolve(__dirname, 'public', 'service-worker.js'), to: paths.build },
+          { from: path.resolve(__dirname, '../../public', 'service-worker.js'), to: paths.build },
         ],
       }),
     );
