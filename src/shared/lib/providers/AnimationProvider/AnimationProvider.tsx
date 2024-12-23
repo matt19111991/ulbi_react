38fdb7118dc083c6c10a81c3bcceb84a4d2c772a
matt19111991 <raven610@yandex.ru>
2024-12-23 15:21:50 +0300

    Fix AnimationProvider.tsx

--- src/shared/lib/providers/AnimationProvider/AnimationProvider.tsx
+++ src/shared/lib/providers/AnimationProvider/AnimationProvider.tsx
@@ -1,10 +1,15 @@
 import { createContext, useContext, useEffect, useMemo, useRef, useState } from 'react';
-import type { MutableRefObject, ReactNode } from 'react';
+import type { ReactNode, RefObject } from 'react';
 
 // для корректной типизации библиотек
+import type { AnimatedComponent, WithAnimated } from '@react-spring/web'; // отдельный импорт для корректной работы с 'React v.19'
 type GestureType = typeof import('@use-gesture/react'); // библиотека для свайпов, тача, "drag'n'drop"
 type SpringType = typeof import('@react-spring/web'); // библиотека для анимаций
 // необходимо дополнительно установить библиотеку '@react-spring/rafz'
+
+// для корректной работы с 'React v.19'
+type SpringTypeWithTypedAnimationComponent = SpringType &
+  Record<'a', WithAnimated & Record<'div', AnimatedComponent<'div'>>>;
 
 interface AnimationContextPayload {
   /**
@@ -15,7 +20,7 @@ interface AnimationContextPayload {
   /**
    * Анимация
    */
-  Spring?: SpringType;
+  Spring?: SpringTypeWithTypedAnimationComponent;
 
   /**
    * Компонент с анимацией уже загружен?
@@ -50,15 +55,15 @@ export const AnimationProvider = ({ children }: { children: ReactNode }) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   // используем 'refs', чтобы был доступ к значениям без лишних перерисовок
-  const GestureRef: MutableRefObject<GestureType | undefined> = useRef(); // различные варианты
-  const SpringRef = useRef<SpringType>(); // типизации 'ref'
+  const GestureRef: RefObject<GestureType | undefined> = useRef(undefined); // различные варианты
+  const SpringRef = useRef<SpringTypeWithTypedAnimationComponent>(undefined); // типизации 'ref'
 
   useEffect(() => {
     getAsyncAnimationModules()
       .then(([SpringResolved, GestureResolved]) => {
         // сохраняем результаты импортов (это сами библиотеки)
         GestureRef.current = GestureResolved;
-        SpringRef.current = SpringResolved;
+        SpringRef.current = SpringResolved as unknown as SpringTypeWithTypedAnimationComponent;
 
         setIsLoaded(true);
       })
